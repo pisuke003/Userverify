b6a9c59e5c1ac434c45c1116ae0b33d8907f73ad
pisuke003 <kashyapavi528@gmail.com>
2025-07-02 14:20:07 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home.jsx';
 import Login from './pages/login.jsx';
-import VerificationEmail from './pages/emailverify.jsx'; 
-import ResetPassword from './pages/emailverify.jsx';
-  import { ToastContainer } from 'react-toastify';
+import VerificationEmail from './pages/emailverify.jsx';
+import ResetPassword from './pages/ResetPassword.jsx';
+import { ToastContainer } from 'react-toastify';
+
 function App() {
   return (
     <div>
-     < ToastContainer/>
+      <ToastContainer />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/email-verify' element={<VerificationEmail />} />
-         <Route path='/forget-password' element={<ResetPassword />} />
+        <Route path='/forget-password' element={<ResetPassword />} />
       </Routes>
     </div>
   );
